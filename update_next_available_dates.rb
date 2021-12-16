9042483da66e49ff73f7fa2505cc21e35a825b1e
Jeff Langr <jeff@langrsoft.com>
2021-12-15 20:10:57 -0700

    a bit more info when trying to update

--- update_next_available_dates.rb
+++ update_next_available_dates.rb
@@ -1,8 +1,9 @@
 require './next_available_dates.rb'
 
 updater = NextAvailableDates.new
-if ARGV.length != 2
+if ARGV.length < 2
   puts "must supply pubmob dir"
+  puts "pubmob_dir set to #{ARGV[0]}"
 else
   updater.bearer_token = ARGV[1]
   updater.pubmob_directory = ARGV[0]
